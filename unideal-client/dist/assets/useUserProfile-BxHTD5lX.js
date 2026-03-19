dc482bbcc18c04b16ed810a014d61b4f92b84566
Raaj Patkar <raajpatkar@gmail.com>
2026-03-19 19:08:55 +0530

    Unwanted Cors Error

--- unideal-client/dist/assets/useUserProfile-BxHTD5lX.js
+++
@@ -1,1 +0,0 @@
-import{v as r,I as s,o as u}from"./index-afO8EdP7.js";function n(){const{isSignedIn:e}=r();return s({queryKey:["userProfile"],queryFn:()=>u.get("/api/users/me"),enabled:!!e})}export{n as u};
